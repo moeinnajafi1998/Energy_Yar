e0adfe4d5dbf78b046b04e7fe2b0b60b8fd659e5
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-12-29 12:42:18 +0330

    improve model

--- OrderApp/permissions.py
+++ OrderApp/permissions.py
@@ -2,8 +2,6 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminOrCustomer(BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == 'admin':
-            return True
-        if request.user.role == 'customer' and request.user.id == view.kwargs['customer_id']:
+        if request.user.role == 'admin' or request.user.role == 'customer':
             return True
         return False

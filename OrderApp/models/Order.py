0584cf4f8721dac0c5bbb03419d186e3f96911af
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-12-29 11:57:02 +0330

    improvements on Order model

--- OrderApp/models/Order.py
+++ OrderApp/models/Order.py
@@ -2,12 +2,12 @@ from django.db import models
 from .CustomUser import CustomUser
 
 class Order(models.Model):
-    product_name = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=255,unique=True)
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    customer = models.ForeignKey(CustomUser, to_field='username',on_delete=models.CASCADE, related_name='orders')
 
     def __str__(self):
         return f'{self.product_name} - {self.customer.username}'

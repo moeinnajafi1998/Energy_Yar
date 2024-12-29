4d26cc263f2ba56e9383158d3186b5b5abb4b878
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-12-29 11:57:21 +0330

    create order API

--- OrderApp/urls.py
+++ OrderApp/urls.py
@@ -1,6 +1,11 @@
 from .views import *
+from django.urls import path
 
 urlpatterns = [
-
+    # path('orders/', views.OrderListView.as_view(), name='order-list'),
+    path('orders/create/', OrderCreateView.as_view(), name='order-create'),
+    # path('orders/<int:pk>/', views.OrderRetrieveView.as_view(), name='order-retrieve'),
+    # path('orders/<int:pk>/update/', views.OrderUpdateView.as_view(), name='order-update'),
+    # path('orders/<int:pk>/delete/', views.OrderDeleteView.as_view(), name='order-delete'),
 
 ]
--- OrderApp/views.py
+++ OrderApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from .models import Order
+from .serializers import OrderSerializer
+from .permissions import IsAdminOrCustomer 
+
+class OrderCreateView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated,IsAdminOrCustomer]
+    authentication_classes = [TokenAuthentication]  
+    
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
